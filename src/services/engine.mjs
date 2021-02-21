982d56a8bc568d4a63a0c112c78cacb9fe3bd762
Alejandro González-Pérez <alejandro.gonzalez-perez@teliacompany.com>
2021-02-21 15:20:32 +0100

    Switch between different types of promotions. Pending deciding which one will take priority for mutual exclusion

--- src/services/engine.mjs
+++ src/services/engine.mjs
@@ -1,6 +1,20 @@
 function process(promotions, basket) {
     //TODO: Iterate over promotions and match items in basket
 
+    for (const promo of promotions) {
+        switch (promo.type) {
+            //Volume discount
+            case "VOLUME":
+                break;
+                //Bundle discount
+            case "BUNDLE":
+                break;
+
+            case "PERCENTAGE":
+                break;
+        }
+    }
+
     return null;
 }
 
