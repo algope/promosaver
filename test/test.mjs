38575e0ebe02c52b41a632efa58d0be5a6b9871d
Alejandro González-Pérez <alejandro.gonzalez-perez@teliacompany.com>
2021-02-21 15:05:44 +0100

    Generated sample test scenarios

---
+++ test/test.mjs
@@ -0,0 +1,32 @@
+import {
+    expect
+} from 'chai';
+import {
+    readFileSync
+} from 'fs';
+
+
+const filePromotions = readFileSync('./src/config/promotions.json');
+const promotions = JSON.parse(filePromotions);
+
+
+describe("Correct promotion calculation test", () => {
+
+    it("should return 100 for scenario A", () => {
+        let basket = ["A", "B", "C"];
+        let result = process(promotions, basket)
+        expect(result).to.equal(100)
+    })
+
+    it("should return 370 for scenario B", () => {
+        let basket = ["A", "A", "A", "A", "A", "B", "B", "B", "B", "B", "C"]
+        let result = process(promotions, basket)
+        expect(result).to.equal(370)
+    })
+
+    it("should return 280 for scenario C", () => {
+        let basket = ["A", "A", "A", "B", "B", "B", "B", "B", "C", "D"]
+        let result = process(promotions, basket)
+        expect(result).to.equal(280)
+    })
+})
