28fdab9daaf56cadf19999e2e28ef372ed4c574b
Alejandro González-Pérez <alejandro.gonzalez-perez@teliacompany.com>
2021-02-21 15:17:06 +0100

    Basic entrypoint

---
+++ index.mjs
@@ -0,0 +1,24 @@
+import {
+    readFileSync
+} from 'fs';
+import {
+    process
+} from './src/services/engine.mjs'
+
+let filePromos = readFileSync('./src/config/promotions.json');
+let promos = JSON.parse(filePromos);
+
+let scenarioA = ["A", "B", "C"];
+let subtotalA = 100;
+
+let scenarioB = ["A", "A", "A", "A", "A", "B", "B", "B", "B", "B", "C"]
+let subtotalB = 420
+
+let scenarioC = ["A", "A", "A", "B", "B", "B", "B", "B", "C", "D"]
+let subtotalC = 335
+
+console.log(">Welcome to the promotion engine.")
+console.log("  Currently active promotions: ")
+for (const promo of promos) {
+    console.log("   -" + promo.description)
+}
